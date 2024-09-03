2bdae53a4ed97d5af3ac8049482bae7c66cb559f
songwqs <songwqs@gmail.com>
2024-09-03 09:04:23 +0800

    Update whisper2.php

--- view/whisper2.php
+++ view/whisper2.php
@@ -7,8 +7,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.min.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.preview.css"/>
+    <link rel="stylesheet" href="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.min.css"/>
+    <link rel="stylesheet" href="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.preview.css"/>
 
     <title>Whisper text!</title>
 </head>
@@ -29,10 +29,10 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/lib/marked.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/lib/prettify.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/lib/flowchart.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/editor.md@1.5.0/editormd.min.js"></script>
+<script src="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/lib/marked.min.js"></script>
+<script src="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/lib/prettify.min.js"></script>
+<script src="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/lib/flowchart.min.js"></script>
+<script src="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/editormd.min.js"></script>
 
 <script>
 
