45af8ed7778bc9cf50e2d9958e17a8d3f84a7194
songwqs <songwqs@gmail.com>
2024-09-03 14:44:17 +0800

    Update whisper2.php

--- view/whisper2.php
+++ view/whisper2.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.min.css"/>
     <link rel="stylesheet" href="https://testingcf.jsdelivr.net/npm/editor.md@1.5.0/css/editormd.preview.css"/>
